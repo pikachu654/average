72cf030b805c8c998a764ce4ab8b7a57ffd2c0e2
pikachu654 <angela.zhong9@gmail.com>
2023-07-24 13:55:52 +1000

    added a .txt file for output

--- HelloWorld/helloworld2.cpp
+++ HelloWorld/helloworld2.cpp
@@ -6,15 +6,20 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	int average;
+	ofstream out;
 	std::cout << "Hi! here is the average of the 3 numbers\n";
 
   	int val;
   	if (argc >= 2){    
+		out.open(filename.c_str());
+		std::out << "Hi! here is the average of the 3 numbers\n";
 	 	for (int i = 1; i < argc; i++){
 			val = atoi(argv[i]);
 			average += val;
   		}	
 		cout <<"Average " << average << endl;
+		out <<"Average " << average << endl;
+		out.close();
   	} else
   	return 0;
 }
